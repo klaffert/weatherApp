945ade96a37d90c69167f889d2964837797262d2
Kelliann Lafferty <lafferty.kelliann@gmail.com>
2020-06-24 10:01:19 -0700

    confirm gitignore

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const API_KEY = config.API_KEY;
-fetch('http://api.weatherstack.com/current?access_key=API_KEY&query=Altoona')
+let KEY = config.API_KEY;
+
+fetch(`http://api.weatherstack.com/current?access_key=${KEY}&query=Seattle`)
   .then((response) => response.json())
   .then((data) => console.log(data));

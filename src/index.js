f77851b37981cb5e3966859350e5e27c7f0a48f4
Kelliann Lafferty <lafferty.kelliann@gmail.com>
2020-06-24 11:26:52 -0700

    workgin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
-let KEY = config.API_KEY;
+const KEY = config.API_KEY;
+const form = document.querySelector('.banner form');
+const input = document.querySelector('.banner input');
+const errorMessage = document.querySelector('.banner .errorMessage');
+const list = document.querySelector('.fetch-section .cities');
 
-fetch(`http://api.weatherstack.com/current?access_key=${KEY}&query=Seattle`)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const listItems = list.querySelectorAll('.fetch-section .city');
+  const inputVal = input.value;
+
+  const url = `http://api.weatherstack.com/current?access_key=${KEY}&query=${inputVal}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // const icon = `${data.current.weatherIcons[0]}`;
+      const li = document.createElement('li');
+      li.classList.add('city');
+      const markup = `
+          <h2 class="city-name" data-name="${data.location.name},${
+        data.location.country
+      }">
+            <span>${data.location.name}</span>
+            <sup>${data.location.country}</sup>
+          </h2>
+   <div class="city-temp">${Math.round(
+     data.current.temperature
+   )}<sup>°C</sup></div>
+        `;
+      li.innerHTML = markup;
+      list.appendChild(li);
+    })
+    .catch(() => {
+      errorMessage.textContent = 'Please search for a valid city!';
+    });
+  errorMessage.textContent = '';
+  form.reset();
+  input.focus();
+});

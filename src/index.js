fa5dcdb059d7bff741239ea28ac698106d20ac56
Kelliann Lafferty <lafferty.kelliann@gmail.com>
2020-06-24 13:15:48 -0700

    truncated temp

--- src/index.js
+++ src/index.js
@@ -19,35 +19,28 @@ form.addEventListener('submit', (event) => {
   const listItems = list.querySelectorAll('.fetch-section .city');
   const inputVal = input.value;
 
-  function convertToF(celsius) {
-    // make the given fahrenheit variable equal the given celsius value
-    // multiply the given celsius value by 9/5 then add 32
-    let c = `${Math.round(data.current.temperature)}`;
-    let fahrenheit = (c * 9) / 5 + 32;
-    // return the variable fahrenheit as the answer
-    return fahrenheit;
-  }
-
   const url = `http://api.weatherstack.com/current?access_key=${KEY}&query=${inputVal}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       const li = document.createElement('li');
       li.classList.add('city');
-      const celsius = `${Math.round(data.current.temperature)}`;
-      const fahrenheit = (celsius * 9) / 5 + 32;
-      const markup = `
-          <h2 class="city-name" data-name="${data.location.name},${data.location.region}">
+
+      //Convert C to F
+      const celsius = `${Math.trunc(data.current.temperature)}`;
+      const fahrenheit = Math.trunc((celsius * 9) / 5 + 32);
+
+      const content = `
+          <h2 class="city-name" data-name="${data.location.name}">
             <span>${data.location.name}</span>
-            <sup>${data.location.region}</sup>
           </h2>
-   <div class="city-temp">${fahrenheit}<sup>°F</sup></div>
-   <figure>
-   <img class="city-icon" src=${data.current.weather_icons[0]}>
-   <figcaption>${data.current.weather_descriptions[0]}></figcaption>
-   </figure>
+          <div class="city-temp">${fahrenheit}<sup>°F</sup></div>
+          <figure>
+            <img class="city-icon" src=${data.current.weather_icons[0]}>
+              <figcaption>${data.current.weather_descriptions[0]}</figcaption>
+          </figure>
         `;
-      li.innerHTML = markup;
+      li.innerHTML = content;
       list.appendChild(li);
     })
     .catch(() => {
